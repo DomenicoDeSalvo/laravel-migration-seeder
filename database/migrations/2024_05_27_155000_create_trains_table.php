091c6851a4ecc7a20ad1c0b202ae52cdcf9d474f
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-27 18:47:31 +0200

    Added table

---
+++ database/migrations/2024_05_27_155000_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 100);
+            $table->string('Stazione Partenza', 100);
+            $table->string('Stazione Destinazione', 100);
+            $table->dateTime('Orario Partenza', $precision = 0);
+            $table->dateTime('Orario Arrivo', $precision = 0);
+            $table->string('Codice Treno', 8);
+            $table->unsignedTinyInteger('Numero Carrozze');
+            $table->unsignedTinyInteger('In Orario');
+            $table->unsignedTinyInteger('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
