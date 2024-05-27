f4f68392503a6bc8d75e2f36b6d24e34dee9f583
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-27 21:35:12 +0200

    Trains are now displayed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,32 @@
 @extends('layouts.app')
 
+@section('title','Treni')
 @section('content')
     <div class="container text-center">
-        <h1>HOME</h1>
+        <h2>Treni</h2> 
+    </div>
+    <div class="container">
+        <table class='table'>
+            <thead>
+                <tr>
+                    <th>Treno</th>
+                    <th>Partenza</th>
+                    <th>Destinazione</th>
+                    <th>Data Partenza</th>
+                    <th>Data Arrivo</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                        <td>{{$train -> train_code}}</td>
+                        <td>{{$train -> departure}}</td>
+                        <td>{{$train -> destination}}</td>
+                        <td>{{$train -> departure_time}}</td>
+                        <td>{{$train -> arrival_time}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
